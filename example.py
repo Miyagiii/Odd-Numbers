75d9fe64d003c7b5c6b82d696ff1be5400001dfc
TheSpaceCowboy <deadpool42534@gmail.com>
2017-12-29 11:06:46 +0000

    Initial commit
    Finds the odd numbers before x.

---
+++ example.py
@@ -0,0 +1,4 @@
+import odd,random
+x = random.randint(0,1000) # Gets a number to put into the algorithm
+print("Finding the odd numbers below: "+str(x)) # Tells the user what number it's looking for
+print(odd.findOdd(x)) # Finds x
---
+++ odd.py
@@ -0,0 +1,12 @@
+# Coded by TheSpaceCowboy
+# Github: https://www.github.com/TheSpaceCowboy
+# Date: 29/12/17
+def findOdd(num :int):
+    numbers = [1] # Stores the numbers and starts from 1
+    while True: # Itterates through every number up until num
+        if(numbers[-1]+2 <= num): # Adding 2 to the output of the algorithm will always equal an odd number
+            numbers.append(numbers[-1]+2) # Appends the number to the array 
+        else:
+            return numbers # Returns the array of odd numbers
+        
+
